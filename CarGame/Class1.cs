27167929f02e0eb95b08039fe489a8f9e0efc4d8
Marcus Lönneborg <marcuslonneborg@gmail.com>
2015-09-23 23:02:14 +0200

    first commit

---
+++ CarGame/Class1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace CarGameEngine
+{
+    [DataContract(Name="DriveTrain")]
+    public enum DriveTrainType
+    {
+        [EnumMember]
+        FrontWheelDrive = 0,
+        [EnumMember]
+        RearWheelDrive=1,
+        [EnumMember]
+        AllWheelDrive=2
+    };
+
+    public static class CarTools 
+    {
+        public static void SaveCarXml(string filename, Car carToSave)
+        {
+            Stream stream = File.Open(filename, FileMode.Create);
+            DataContractSerializer ser =
+                new DataContractSerializer(typeof(Car));
+            ser.WriteObject(stream, carToSave);
+            stream.Close();
+        }
+        public static Car LoadCarXml(string filename)
+        {
+            Stream stream = File.Open(filename, FileMode.Open);
+            DataContractSerializer ser =
+                new DataContractSerializer(typeof(Car));
+            Car carToLoad = (Car)ser.ReadObject(stream);
+            stream.Close();
+            return carToLoad;
+        }
+
+
+    }
+
+    [DataContract]
+    public class Car : AerodynamicShape
+    {
+       [DataMember]
+        private DriveTrainType _driveTrain;
+        [DataMember]
+        private Engine _engine;
+        [DataMember]
+        private string _name = String.Empty;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    _name = "Default";
+                else
+                    _name = value;
+            }
+        }
+        public Engine engine
+        {
+            get
+            {
+                return _engine;
+            }
+        }
+
+        public Car()
+        {
+            _engine = new Engine();
+            _driveTrain = DriveTrainType.RearWheelDrive;
+            _name = "Default";
+            base.Mass = 1250;
+
+
+        }
+        public float GetDrag(float Speed)
+        {
+            return base.GetDragForce(Speed);
+        }
+        public void ChangeDriveTrain(DriveTrainType driveTrain)
+        {
+            _driveTrain = driveTrain;
+        }
+    }
+    [DataContract]
+    public class AerodynamicShape
+    {
+        private const float _dragCoefficient = 0.31f;
+        private const float _rollingFrictionCoefficient = 0.015f;
+        private const float _airDensity = 1.2f;
+        private const Int32 _metersPerKiloMeter = 1000;
+        private const Int32 _secondsPerHour = 3600;
+        private float _frontalArea = 1.94f;
+        private const Int32 _maxMass = 5000;
+        [DataMember]
+        private Int32 _mass = 1323;
+        protected Int32 Mass
+        {
+            get { return _mass; }
+            set { if (value > 0 && value < _maxMass) _mass = value; }
+        }
+        protected float GetDragForce(float SpeedInKmPerHour)
+        {
+            float speedInMeterPerSecond = SpeedInKmPerHour * _metersPerKiloMeter / _secondsPerHour;
+            return 1f / 2f * (_dragCoefficient * _airDensity * _frontalArea * speedInMeterPerSecond * speedInMeterPerSecond);
+
+
+        }
+
+    }
+    [DataContract]
+    public class Engine
+    {
+        [DataMember]
+        private Int32[] _torqueCurve;
+
+
+        public Int32 maxRPM
+        {
+            get
+            {
+                for (int i = _torqueCurve.Length - 1; i >= 0; i--)
+                {
+                    if (_torqueCurve[i] > 0) return Math.Min(i + 2, _torqueCurve.Length) * 500;
+                }
+                return 0;
+            }
+        }
+
+        public Engine()
+        {
+            _torqueCurve = new Int32[] { 0, 80, 190, 210, 225, 230, 235, 238, 235, 230, 221, 200, 180, 120, 80, 0, 0, 0, 0, 0 };
+
+
+        }
+        public Int32 GetTorqueNm(Int32 rpm)
+        {
+            var index = rpm / 500;
+            if (index >= 0 && index < _torqueCurve.Length)
+            {
+                return _torqueCurve[index];
+            }
+            else return 0;
+        }
+        public Int32 GetPowerWatt(Int32 rpm)
+        {
+            var torque = GetTorqueNm(rpm);
+            return (int)Math.Round(rpm / 60 * 2 * Math.PI * torque);
+
+        }
+        public Int32 GetPowerHP(Int32 rpm)
+        {
+            var watt = GetPowerWatt(rpm);
+            return (int)Math.Round(watt / 745.699872f);
+        }
+    }
+}
---
+++ CarGameTest/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarGameEngine;
+
+namespace CarGameTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            c.Name = "Volvo";
+            c.ChangeDriveTrain(DriveTrainType.AllWheelDrive);
+            PrintCarInfo(c);
+            CarTools.SaveCarXml("test.xml", c);
+            c = null;
+            c = CarTools.LoadCarXml("test.xml");
+            PrintCarInfo(c);
+            Console.ReadLine();
+        }
+
+
+
+
+        static void PrintCarInfo(Car car)
+        {
+            Console.WriteLine("Printing info for car: {0}", car.Name);
+
+            for (int i = 0; i < car.engine.maxRPM; i += 500)
+            {
+                Console.WriteLine("Rpm: {0}\tTorque(Nm): {1}\t\tPower(kW): {2}\t\tPower(HP): {3}", i, car.engine.GetTorqueNm(i), car.engine.GetPowerWatt(i) / 1000, car.engine.GetPowerHP(i));
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 290; i += 10)
+            {
+                Console.WriteLine("Speed(Km/h): {0}\t Drag(Nm): {1}", i, car.GetDrag(i));
+            }
+        }
+    }
+}
